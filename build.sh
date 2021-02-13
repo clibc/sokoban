82e0fa6b7759d0587ed7122791683c6810fa56fa
62bit <shirvani.abddullayev@gmail.com>
2021-02-13 23:15:15 +0100

    if -d output/

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ includes="-I ../dependencies/ \
 	  -I ../src/"
 libs="-lglfw -lGL"
 
+if [ ! -d "./output/" ]
+then 
+	mkdir ./output/
+fi
+
 pushd ./output/
 time gcc $flags $includes $files -lm $libs
 popd
-
