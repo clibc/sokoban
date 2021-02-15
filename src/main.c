d15745cf8e3ccc18a93244611797a467695a2aad
62bit <shirvani.abddullayev@gmail.com>
2021-02-15 13:08:45 +0100

    t

--- src/main.c
+++ src/main.c
@@ -28,18 +28,20 @@ int main()
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f};
 
-    vertexbuffer vb = create_vertexbuffer(&vertices[0], sizeof(vertices));
-
     shader simpleShader = create_shader("./src/shaders/vertex.vs",
                                         "./src/shaders/fragment.vs");
-    //set_vertexbuffer_attibutes(&vb, 0, 3, 3 * sizeof(float), (void *)0);
+    glUseProgram(simpleShader.programID);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vb.bufferID);
+    unsigned int vb;
+    glGenBuffers(1, &vb);
+    glBindBuffer(GL_ARRAY_BUFFER, vb);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     printf("%i\n", glGetError());
 
-    glEnableVertexAttribArray(0);
-    printf("%i\n", glGetError());
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    printf("%i\n", glGetError());
+    glEnableVertexAttribArray(0);
     printf("%i\n", glGetError());
 
     while (!glfwWindowShouldClose(window))
