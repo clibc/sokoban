975f5bbbece76f01378a7dc39d4435998e5a41cf
62bit <shirvani.abddullayev@gmail.com>
2021-03-04 12:52:07 +0100

    renamed build for windows

---
+++ wbuild.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set files=..\src\*.c
+set flags=/nologo /Fe.\a.exe /EHsc /Zi /MP6
+set libs=opengl32.lib user32.lib ..\dependencies\GLFW\libs\glfw3dll.lib ..\dependencies\GLFW\libs\glew32.lib
+set includes=/I ..\dependencies\ /I ..\src\
+
+if not exist output\ (
+   mkdir output\
+)
+
+pushd output
+    cl %flags% %files% %includes% /link %libs% /SUBSYSTEM:CONSOLE
+popd 
